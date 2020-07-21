1d262827caed0d8f54ba5dd8d153bdfe5e2d0f98
Mirko Albanese <mirko.albanese.94@gmail.com>
2020-07-21 13:13:11 +0200

    Add files via upload

--- 1DwaveSolver.cpp
+++ 1DwaveSolver.cpp
@@ -1,113 +1,215 @@
-#include "ariadne"
-
 #include "numeric/numeric.hpp"
-#include "function/function.hpp"
+#include "numeric/real.hpp"
+#include "algebra/algebra.hpp"
+#include "algebra/vector.hpp"
+#include "utility/array.hpp"
+//#include "function/function.hpp"
 
-#include <cmath>
-#include <vector>
-#include <time.h>
+//#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <unistd.h>
 
-#include "matplotlibcpp.h"
+#include "gnuplot-iostream.h"
 
-template<typename T>
-std::vector<double> linspace(T start, T end, int num)
+using namespace Ariadne;
+using namespace std;
+using  std::ofstream;
+
+Array<Real> linspace(Real end, Real num)
 {
-    std::vector<double> linspaced;
+    Array<Real> linspaced(num.get_d());
 
-    double startIn = static_cast<double>(start);
-    double endIn = static_cast<double>(end);
-    double numIn = static_cast<double>(num);
+    //DimensionType numIn = num;
 
-    if (numIn == 0)
+    if (num.get_d() == 0)
         return linspaced;
-    if (numIn == 1)
+    if (num.get_d() == 1)
     {
-        linespaced.push_back(startIn);
+        linspaced[0] = end;
         return linspaced;
     }
 
-    double delta = (endIn - startIn)/(num - 1);
+    Real delta = end/(num - 1);
 
-    for (int i = 0; i < end - 1; i++)
+    for (SizeType i = 0; i < (num.get_d() - 1); i++)
     {
-        linspaced.push_back(startIn + delta*i);
+        linspaced[i] = 0 + delta*i;
     }
-    linspaced.push_back(end);
+    linspaced[num.get_d() - 1] = end;
        
     return linspaced;
 }
 
 int main(int argc, const char** argv) {
-    
+
+    Real Nx = 41;                // # point in space
+
+    String tempStr;
+
+    tempStr = to_string(Nx.get_d() - 1);
+
+    Real Zero = 0;
+
     //Solution variable
-    std::vector<float> u(Nx, 0);   // Next value
-    std::vector<float> u_1(Nx, 0); // Current value
-    std::vector<float> u_2(Nx, 0); // Past value
+    Array<Real> u(Nx.get_d());   // Next value
+    Array<Real> u_1(Nx.get_d()); // Current value
+    Array<Real> u_2(Nx.get_d()); // Past value
 
-    Real L = 0.5;   // m - Lenght
-    Real T = 10;     // s - Simulation Time
-    Real c = 1.5;   // m/s - wave velocity
+    Array<double> sol1D(Nx.get_d());
 
-    C = 0.75;        // Courant Number
-    C2 = pow(C,2);
+    Real L = 1.0_q;   // m - Lenght
+    Real x0 = 0.8_q*L;          // Triangular wave - point of max amplitude
+    Real freq = 220;        // Oscillation frequency
+    Real omega = 2*pi*freq;
+    Real wavelength = 2*L;  //wavelength
+    Real c = freq*wavelength;   // m/s - wave velocity
+    Real amp = 0.5_q;           // amplitude
+    Real damp = 15000;
 
-    int Nx = 100                        // # point in space
-    int Nt = round(T/dt);               // # point in time
+    //Real numPeriod = 1;
+    //Real T = (2*pi/omega);
+    Real T = wavelength/c;            
 
-    auto space = linspace(0, L, Nx);    // Mesh in space
+    Real C = 0.1_q;        // Courant Number <= 1
+    Real C2 = pow(C, 2);
+
+    Array<Real> space = linspace(L, Nx);    // Mesh in space
+
     Real dx = space[1] - space[0];
+    Real dt = C*dx/c;
+    Real Nt = round(T/dt);
+    //SizeType _Nt = Nt.get_d();              
+    auto time = linspace(Nt*dt, Nt);
 
-    dt = C*dx/c;
-    auto time = linspace(0, Nt*dt, Nt);
+    Real k = 2*pi/wavelength;           // wave number
 
-    auto PosInitCond = [](int x){return sin(pi/L*x)};  // Set function initial position condition
-    auto VelInitCond = [](int x){return 0};            // Set function initial velocity position
-    auto source = [](int x, int t){return 0};
+    // Set function initial position condition
+    auto PosInitCond = [&](Real x, Real t){return sin(k*x)*cos(omega*t);};  //Steady-state wave
+/*    
+    auto PosInitTri = [&](Real x){                          //Triangular
+        if (x.get_d() < x0.get_d())
+            return amp*(x/x0);
+        else
+            return amp*(L - x)/(L - x0);
+    };
+*/
+    //auto VelInitCond = [&](Real x){return 0;};            // Set function initial velocity position
+    //auto source = [&](Real x, Real t){return 0;};
+
+    Gnuplot gp;
+
+   
+    gp << "set term gif animate\n";
+    gp << "set output 'animate.gif'\n";
+    gp << "set xlabel 'X'\n";
+    gp << "set ylabel 'Amplitude'\n";
+
+/*
+    ofstream outdata;
+    outdata.open("data.dat");
+    if (!outdata)
+    {
+        std::cerr << "Error: file couldn't be opened" << std::endl;
+        exit(1);     
+    }
+*/
 
     // Set the initial condition
-    for (int i = 0; i <= Nx; i++)
+    for (SizeType i = 0; i < Nx.get_d(); i++)
     {
-        u_1[i] = InitCond(space[i]);   // Set initial condition
+        u_1[i] = PosInitCond(space[i], Zero);   // Set initial condition - Steady-state 
+        //u_1[i] = PosInitTri(space[i]);
+        sol1D[i] = u_1[i].get_d();
+
+        //outdata << u_1[i].get_d() << std::endl;
     }
+
+    sleep(1);
+    //gp << "plot 'data.dat' \n";
+    //outdata.close();
+
+    gp << "plot [0:" << tempStr << "][-1:1] '-' with linespoints ls 1\n";
+    gp.send1d(sol1D);
+    //gp.flush();
 
     // Set Boundary Condition
-    u[0] = 0;
-    u[Nx] = 0;
+    u[0] = Zero;
+    u[Nx.get_d() - 1] = Zero;
+
+    sol1D[0] = Zero.get_d();
+    sol1D[Nx.get_d() - 1] = Zero.get_d();
 
     // Set first time step
-    int n = 0;
-    for (int i = 1; i < Nx; i++)
+    SizeType n = 0;
+    for (SizeType i = 1; i < Nx.get_d() - 1; i++)
     {
-        u[i] = u_1[i] + dt*VelInitCond(space[i]) 
-            + 0.5*C2*(u_1[i-1] - 2*u_1[i] + u_1[i+1]) 
-                + 0.5*pow(dt, 2)*source(space[i], time[n]);
+        u[i] = u_1[i] //+ dt*VelInitCond(space[i]) 
+            - 0.5_q*C2*(u_1[i-1] - 2*u_1[i] + u_1[i+1]);
+                //+ 0.5_q*pow(dt, 2)*source(space[i], time[n]);
+        sol1D[i] = u[i].get_d();
     }
+    u[0] = Zero;
+    u[Nx.get_d() - 1] = Zero;
 
+    sol1D[0] = Zero.get_d();
+    sol1D[Nx.get_d() - 1] = Zero.get_d();
+
+    gp << "plot [0:" << tempStr << "][-1:1] '-' with linespoints ls 1\n";
+    gp.send1d(sol1D);
+
+/*
+    //gp.flush();
+    gp << "plot '-' \n";
+    gp.send1d(sol1D);
+*/
+    // Before next step switch variables
     u_2 = u_1;
     u_1 = u;
 
     // For each time step
-    for (int n = 1; n <= Nt; n++)
+    for (n = 1; n < Nt.get_d(); n++)
     {
         // Update the inner points
-        for (i = 1; i < Nx; i++)
+        for (SizeType i = 1; i < Nx.get_d() - 1; i++)
         {
-            u[i] = -u_2[i] + 2*u_1[i] 
-                + C2*(u_1[i-1] - 2*u_1[i] + u_1[i+1]) 
-                    + pow(dt, 2)*source(space[i], time[n]);
+            u[i] = (1/(1+0.5_q*damp*dt))*(-(u_2[i]) + 1.1_q*u_1[i] + damp*dt/2*u_2[i] +
+                + C2*(u_1[i-1] - 2*u_1[i] + u_1[i+1]));
+                    //+ pow(dt, 2)*source(space[i], time[n]));
+            //std::cout << u[i].get_d() << "\n";
+
+            sol1D[i] = u[i].get_d();
         }
+
+        //cout << endl;
         // Insert Boundary Condition
-        u[0] = 0;
-        u[Nx] = 0;
+        u[0] = Zero;
+        u[Nx.get_d() - 1] = Zero;
+        sol1D[0] = Zero.get_d();
+        sol1D[Nx.get_d() - 1] = Zero.get_d();
+/*
+        gp << "plot '-' [0:100]\n";
+        gp.send1d(sol1D);
+        gp.flush();
+        
+*/
+        gp << "plot [0:" << tempStr << "][-1:1] '-' with linespoints ls 1\n";
+        gp.send1d(sol1D);
+        sleep(0.2);
 
         // Before next step switch variables
         u_2 = u_1;
         u_1 = u;
-        
+/*        
+        // Plotting w/ GNUPLOT
+        gp << "plot '-' with lines 'ez' \n";
+        gp.send1d(u);
+        gp.flush();
+*/
     }
-
-    
-    
+    //gp << "set output\n";
 
     return 0;
+
 }
